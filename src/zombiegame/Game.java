514d277fb8e33941e514b5a0f470fa4ecc4ee136
Reid Reininger <charles.reininger@wsu.edu>
2020-03-06 01:09:19 -0800

    Add other zombie build functions

--- src/zombiegame/Game.java
+++ src/zombiegame/Game.java
@@ -15,7 +15,38 @@ public class Game {
         this.zombieBuilder = new ZombieBuilder();
     }
 
+    /*
+    Return new Zombie.
+     */
     private Zombie constructZombie() {
-        this.
+        this.zombieBuilder.buildZombie();
+        return this.zombieBuilder.getZombie();
+    }
+
+    /*
+    Return new cone zombie.
+     */
+    private Zombie constructConeZombie() {
+        this.zombieBuilder.buildZombie();
+        this.zombieBuilder.buildCone();
+        return this.zombieBuilder.getZombie();
+    }
+
+    /*
+    Return new bucket zombie.
+     */
+    private Zombie constructBucketZombie() {
+       this.zombieBuilder.buildZombie();
+       this.zombieBuilder.buildBucket();
+       return this.zombieBuilder.getZombie();
+    }
+
+    /*
+    Return new door zombie.
+     */
+    private Zombie constructDoorZombie() {
+        this.zombieBuilder.buildZombie();
+        this.zombieBuilder.buildDoor();
+        return this.zombieBuilder.getZombie();
     }
 }

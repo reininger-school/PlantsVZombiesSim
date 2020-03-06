a198acc9bafbce544e56276861636989e4238856
Reid Reininger <charles.reininger@wsu.edu>
2020-03-06 00:05:30 -0800

    Add EnemyObject function headers and attributes

--- src/zombiebuilder/EnemyObject.java
+++ src/zombiebuilder/EnemyObject.java
@@ -1,4 +1,39 @@
 package zombiebuilder;
 
-public class EnemyObject {
+import java.util.List;
+
+/*
+Class representing enemy objects. Serves as Component of Composite pattern.
+ */
+public abstract class EnemyObject {
+    private int health;
+    private List<EnemyObject> children;
+
+    /*
+    Take damage.
+     */
+    public int takeDamage(int d) {
+        return 0;
+    }
+
+    /*
+    Return ith child.
+     */
+    public EnemyObject getChild(int child) {
+        return null;
+    }
+
+    /*
+    Actions to perform on death.
+     */
+    public void Die() {
+    }
+
+    /*
+    Add child EnemyObject.
+     */
+    public void add(EnemyObject child) {
+    }
+
+    public void remove(EnemyObject child){}
 }

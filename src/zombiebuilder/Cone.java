f872bd71964e1333310f9115f16a9c17215cb160
Reid Reininger <charles.reininger@wsu.edu>
2020-03-06 00:47:27 -0800

    Add Cone constructor

--- src/zombiebuilder/Cone.java
+++ src/zombiebuilder/Cone.java
@@ -4,4 +4,7 @@ package zombiebuilder;
 Represents a zombie cone accessory. Leaf in composite pattern.
  */
 class Cone extends EnemyObject {
+    public Cone() {
+        this.setHealth(25);
+    }
 }

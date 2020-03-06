39fe57659f02ddf1c402af2091c3eee4bbeb5a77
Reid Reininger <charles.reininger@wsu.edu>
2020-03-06 01:03:02 -0800

    Add Game constructor

--- src/zombiegame/Game.java
+++ src/zombiegame/Game.java
@@ -1,4 +1,21 @@
 package zombiegame;
 
+import zombiegame.zombiebuilder.*;
+
+/*
+Coordinates Zombie game.
+ */
 public class Game {
+    private ZombieBuilder zombieBuilder;
+
+    /*
+    Create new instance of Game.
+     */
+    public Game() {
+        this.zombieBuilder = new ZombieBuilder();
+    }
+
+    private Zombie constructZombie() {
+
+    }
 }
